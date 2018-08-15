c85d5c185e1c7697095975dfb50839dd3a27de58
lijing.feng <lijing.feng@aispeech.com>
2018-08-15 17:12:56 +0800

    change config

---
+++ src/main/java/com/test/model/enums/IntentResult.java
@@ -0,0 +1,23 @@
+package com.test.model.enums;
+
+public enum IntentResult {
+    YES(1, "确认"),
+    NO(0, "否认")
+    ;
+
+    private int code;
+    private String value;
+
+    private IntentResult(int code, String value){
+        this.code = code;
+        this.value = value;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
