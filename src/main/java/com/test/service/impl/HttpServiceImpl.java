7b3478fa5d501544ef7dc724dde5105d458fb267
lijing.feng <lijing.feng@aispeech.com>
2018-08-15 16:58:44 +0800

    implement method

--- src/main/java/com/test/service/impl/HttpServiceImpl.java
+++ src/main/java/com/test/service/impl/HttpServiceImpl.java
@@ -1,17 +1,28 @@
 package com.test.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.test.service.HttpService;
+import com.test.util.HttpUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HttpServiceImpl implements HttpService {
+    Logger log = LoggerFactory.getLogger(HttpServiceImpl.class);
 
     @Value("{$spring.service.url}")
     private String service_url;
 
     @Override
     public String getInfo(String url) {
-        return null;
+        String respContenet = HttpUtils.invokePost(service_url, "");
+        if (respContenet != null){
+            JSONObject jsonObject = JSON.parseObject(respContenet);
+            log.debug(jsonObject.toJSONString());
+        }
+        return respContenet;
     }
 }
--- src/main/java/com/test/service/impl/RestTemplateServiceImpl.java
+++ src/main/java/com/test/service/impl/RestTemplateServiceImpl.java
@@ -1,13 +1,22 @@
 package com.test.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.test.service.RestTemplateService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class RestTemplateServiceImpl implements RestTemplateService {
+    Logger log = LoggerFactory.getLogger(RestTemplateServiceImpl.class);
+
     @Autowired
     public RestTemplate restTemplate;
 
@@ -16,6 +25,16 @@ public class RestTemplateServiceImpl implements RestTemplateService {
 
     @Override
     public String getInfo(String url) {
-        return null;
+        JSONObject req = new JSONObject();
+        req.put("test", "hello world");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+        HttpEntity httpEntity = new HttpEntity(req, headers);
+        String respContenet = restTemplate.postForObject(service_url, httpEntity, String.class);
+        if (respContenet != null){
+            JSONObject jsonObject = JSON.parseObject(respContenet);
+            log.debug(jsonObject.toJSONString());
+        }
+        return respContenet;
     }
 }
